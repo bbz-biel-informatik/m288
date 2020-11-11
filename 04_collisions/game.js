3a36b7004206b8cb5f78a68c08e6f5c69a861b3b
Lukas_Skywalker <lukas.diener@hotmail.com>
2020-11-11 19:42:19 +0100

    update collisions wording

--- 04_collisions/game.js
+++ 04_collisions/game.js
@@ -6,17 +6,18 @@ var gegner4 = document.querySelector('.enemy4')   /* start=2 */
 spieler.style.left = '0px'
 
 function loop() {
-  // Kommentar: sobald der Spieler mit Gegner1 oder 2 kollidiert, ist das Spiel fertig    /* start=4 */
-  if(anyCollision(spieler, [gegner1, gegner2])) {         /* start=4 */
-    alert("Game over!")                                   /* start=4 */
-  }                                                       /* start=4 */
+  // Kommentar: sobald der Spieler mit Gegner1 oder 2 kollidiert, ist das Spiel fertig    /* start=5 */
+  if(anyCollision(spieler, [gegner1, gegner2])) {         /* start=5 */
+    alert("Game over!")                                   /* start=5 */
+    return                                                /* start=5 */
+  }                                                       /* start=5 */
 
-  // Kommentar: sobald der Spieler mit Gegner3 oder 4 kollidiert, werden diese gelöscht   /* start=5 */
-  var collisions = allCollisions(spieler, [gegner3, gegner4])                             /* start=5 */
-  // Kommentar: wir gehen durch alle Kollisionsobjekte durch und löschen sie              /* start=5 */
-  for(var collision of collisions) {                                                      /* start=5 */
-    collision.parentNode.removeChild(collision)                                           /* start=5 */
-  }                                                                                       /* start=5 */
+  // Kommentar: sobald der Spieler mit Gegner3 oder 4 kollidiert, werden diese gelöscht   /* start=6 */
+  var collisions = allCollisions(spieler, [gegner3, gegner4])                             /* start=6 */
+  // Kommentar: wir gehen durch alle Kollisionsobjekte durch und löschen sie              /* start=6 */
+  for(var collision of collisions) {                                                      /* start=6 */
+    collision.parentNode.removeChild(collision)                                           /* start=6 */
+  }                                                                                       /* start=6 */
 
   window.requestAnimationFrame(loop)
 }
